757b01864cd2991d8132ff86d4358d128f882620
RoyerReyes <royreyes2000@hotmail.com>
2026-03-30 00:23:45 -0500

    fix(salsas): fix export name of auth middleware

--- src/routes/salsasRoutes.js
+++ src/routes/salsasRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { getSalsas, toggleSalsa } from '../controllers/salsasController.js';
-import { verificarToken } from '../middlewares/authMiddleware.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
-router.get('/', verificarToken, getSalsas);
-router.put('/:id/toggle', verificarToken, toggleSalsa);
+router.get('/', authMiddleware, getSalsas);
+router.put('/:id/toggle', authMiddleware, toggleSalsa);
 
 export default router;
